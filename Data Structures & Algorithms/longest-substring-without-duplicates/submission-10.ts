995299b690525ffdb655dbcd03d824361085462b
Prabesh Bimali <54845637+PrabeshBimali@users.noreply.github.com>
2026-06-06 20:18:27 +0545

    Add: longest-substring-without-duplicates - submission-10

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-10.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+
+        if(s === "" ) return 0;
+
+        let map = new Map<string, number>();
+        let left = 0;
+        let globalMax = 1;
+        let currentMax = 0;
+
+        for(let i = 0; i < s.length; i++) {
+            if(map.get(s[i]) >= left) {
+                currentMax = 0;
+                left = map.get(s[i]) + 1;
+            } else {
+                currentMax = i - left + 1;
+            }
+                map.set(s[i], i);
+
+
+            console.log(s[i], currentMax, left, map)
+            
+            if(currentMax > globalMax) globalMax = currentMax;
+
+        }
+
+        if(currentMax > globalMax) globalMax = currentMax;
+
+
+        return globalMax;
+    }
+}
