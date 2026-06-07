90e76d74e5ac1a9e35b0c074b149b9c69fddd639
Prabesh Bimali <54845637+PrabeshBimali@users.noreply.github.com>
2026-06-07 13:19:49 +0545

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.ts
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let left = 0;
+        let right = s.length - 1;
+        const sl = s.toLowerCase();
+        while(left < right) {
+
+            let lCode = sl[left].charCodeAt(0);
+            let rCode = sl[right].charCodeAt(0);
+
+            while(!((lCode >= 48 && lCode <= 57) || (lCode >= 97 && lCode <= 122))){
+                left+=1;
+
+                if(left > right) return true;
+
+                lCode = sl[left].charCodeAt(0);
+            }
+
+            while(!((rCode >= 48 && rCode <= 57) || (rCode >= 97 && rCode <= 122))) {
+                right-=1;
+                
+                if(left > right) return true;
+
+                rCode = sl[right].charCodeAt(0);
+            }
+
+            console.log(sl[left], sl[right])
+
+            if(sl[left] !== sl[right]) return false;
+            left += 1;
+            right -= 1;
+        }
+
+        return true;
+    }
+}
