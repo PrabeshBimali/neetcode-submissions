b7ff29d18203004b07c5466912ac3b2702b3ca30
Prabesh Bimali <54845637+PrabeshBimali@users.noreply.github.com>
2026-06-15 20:15:55 +0545

    Add: rotate-array - submission-3

---
+++ Data Structures & Algorithms/rotate-array/submission-3.ts
@@ -0,0 +1,30 @@
+class Solution {
+
+
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    //let
+    rotate(nums: number[], k: number): void {
+        const copyNums = new Array<number>(nums.length);
+        const newK = k % nums.length;
+
+        for(let i = 0; i < nums.length; i++) {
+            if(i === newK) {
+                copyNums[i] = nums[0];
+            } else if(i > newK) {
+                console.log(nums[i - newK])
+                copyNums[i] = nums[i-newK];
+            } else if(i < newK) {
+                copyNums[i] = nums[nums.length - (newK - i)];
+            }
+        }
+
+        for(let i = 0; i < nums.length; i++) {
+            nums[i] = copyNums[i];
+        }
+
+    }
+}
