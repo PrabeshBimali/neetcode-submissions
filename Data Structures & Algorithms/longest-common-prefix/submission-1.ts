09e3daef6a555a9cc0524f2c08f9a5040431a65a
Prabesh Bimali <54845637+PrabeshBimali@users.noreply.github.com>
2026-06-16 23:45:54 +0545

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs: string[]): string {
+        let prefix = strs[0];
+
+        for(let i = 0; i < strs.length; i++) {
+            for(let j = 0; j < prefix.length; j++) {
+                if(prefix[j] !== strs[i][j]) {
+                    prefix = prefix.slice(0, j);
+                }   
+            }
+        }
+        
+        return prefix;
+    }
+}
