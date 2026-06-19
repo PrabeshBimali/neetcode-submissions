b3195558eef2b9da5c6503ac22fc6afa207d2038
Prabesh Bimali <54845637+PrabeshBimali@users.noreply.github.com>
2026-06-19 11:54:21 +0545

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.ts
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} asteroids
+     * @return {number[]}
+     */
+    asteroidCollision(asteroids: number[]): number[] {
+        const stack = new Array<number>();
+
+        for(const n of asteroids) {
+            if(n >= 0) {
+                stack.push(n);
+            } else {
+                let top = stack[stack.length - 1];
+                let isDestroyed = false;
+                while(stack.length > 0 && top > 0) {
+                    if(top < Math.abs(n)) {
+                        stack.pop();
+                        top = stack[stack.length - 1];
+                    } else if(top === Math.abs(n)) {
+                        isDestroyed = true;
+                        stack.pop();
+                        break;
+                    } else {
+                        isDestroyed = true;
+                        break;
+                    }
+                }
+
+                if(!isDestroyed) {
+                    stack.push(n)
+                }
+
+            }
+        }
+
+        return stack;
+    }
+}
