6aa96169e0c456d5d0fba8c8076c8901d9bda627
Prabesh Bimali <54845637+PrabeshBimali@users.noreply.github.com>
2026-06-29 15:37:29 +0545

    Add: kth-largest-element-in-an-array - submission-4

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-4.ts
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    // without sorting
+    findKthLargest(nums: number[], k: number): number {
+        let indexToFind = nums.length - k;
+        let start = 0;
+        let end = nums.length - 1;
+        let leftPointer = -1;
+
+        while(leftPointer !== indexToFind) {
+            leftPointer = start;
+            let index = start;
+            let pivot = nums[end];
+
+            while(index < end) {
+                if(nums[index] <= pivot) {
+                    if(index !== leftPointer) [nums[leftPointer], nums[index]] = [nums[index], nums[leftPointer]];
+                    leftPointer++;
+                }
+                index++;
+            }
+
+            [nums[leftPointer], nums[index]] = [nums[index], nums[leftPointer]];
+
+            if(indexToFind > leftPointer) {
+                start = leftPointer + 1;
+            } else if(indexToFind < leftPointer) {
+                end = leftPointer - 1;
+            }
+        }
+
+        return nums[indexToFind];
+    }
+}
