b6a2d573d20f9127fd89e982b658b76867d54efa
Prabesh Bimali <54845637+PrabeshBimali@users.noreply.github.com>
2026-06-30 22:12:56 +0545

    Add: binary-tree-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-0.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    traversal(node: TreeNode, array: number[]): void {
+        if(node === null) return;
+
+        this.traversal(node.left, array);
+        array.push(node.val);
+        this.traversal(node.right, array);
+    }
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    inorderTraversal(root: TreeNode | null): number[] {
+        let array = new Array<number>();
+        this.traversal(root, array);
+        return array;
+    }
+}
