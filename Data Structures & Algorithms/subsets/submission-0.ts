ac9228aa010fbbe8b6920bb4b4184b624a1a64b9
Prabesh Bimali <54845637+PrabeshBimali@users.noreply.github.com>
2026-07-06 21:31:26 +0545

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.ts
@@ -0,0 +1,26 @@
+class Solution {
+    
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums: number[]): number[][] {
+        const result = new Array<Array<number>>();
+
+        function backtrack(index: number, path: number[]) {
+            if(index === nums.length) {
+                result.push([...path]);
+                return
+            }
+
+            path.push(nums[index]);
+            backtrack(index + 1, path);
+            path.pop();
+            backtrack(index + 1, path);
+        }
+
+        backtrack(0, []);
+
+        return result;
+    }
+}
